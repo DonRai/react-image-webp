65ef63aa2ad2c4bc5059cbd45b358bed6e0015ac
Hassan Hafez <hassan1hafez@gmail.com>
2019-10-27 23:05:07 +0200

    Add support for passing all props to <img /> Fix #35

--- modules/index.js
+++ modules/index.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {isWebpSupported} from './utils';
+import { isWebpSupported } from "./utils";
 
 /**
  * Default class for Image webp
  */
 class Image extends Component {
-    /**
-     * Render jsx to html
-     * @returns {XML}
-     */
-    render() {
-        const { src, webp, alt, title, style, className } = this.props;
-        const webpSupport = isWebpSupported();
-        let image = src;
+  /**
+   * Render jsx to html
+   * @returns {XML}
+   */
+  render() {
+    const { src, webp, alt, title, style, className, ...rest } = this.props;
+    const webpSupport = isWebpSupported();
+    let image = src;
 
-        if (webpSupport === true) {
-            image = webp;
-        }
-
-        return <img src={image} alt={alt} title={title} style={style} className={className} />;
+    if (webpSupport === true) {
+      image = webp;
     }
+
+    return (
+      <img
+        {...rest}
+        src={image}
+        alt={alt}
+        title={title}
+        style={style}
+        className={className}
+      />
+    );
+  }
 }
 
 export default Image;
